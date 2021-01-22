a1659ecf91729e812c7d2890cb41d06858d559bd
lifthoofd <eovermeire@gmail.com>
2021-01-22 17:43:26 +0100

    changed puredata start file

--- rpi/install.sh
+++ rpi/install.sh
@@ -80,7 +80,7 @@ cat /home/$user/wbs/rpi/wbs_processing.service |\
     sed "0, /$env/s//&$xauth/" > $temp_dir/wbs_processing.service
 sudo mv $temp_dir/wbs_processing.service /etc/systemd/system/wbs_processing.service
 
-cmd="\/usr\/bin\/puredata -nogui -nomidi -noadc -alsa -audiooutdev 5 \/home\/$user\/wbs\/PD\/osc_test.pd"
+cmd="\/usr\/bin\/puredata -nogui -nomidi -noadc -alsa -audiooutdev 5 \/home\/$user\/wbs\/PD\/_main.pd"
 xauth="XAUTHORITY=\/home\/$user\/.Xauthority"
 env='Environment="'
 cat /home/$user/wbs/rpi/wbs_puredata.service |\
